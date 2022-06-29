0a12bdce0931c6728d5c4c05a9dae3fd88419bef
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-06-28 22:24:13 -0300

    config: configuração inicial do projeto

---
+++ src/main/java/br/com/alura/App.java
@@ -0,0 +1,10 @@
+package br.com.alura;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var nome = "Antonia";
+        System.out.println( "Hello World, " + nome );
+    }
+}
