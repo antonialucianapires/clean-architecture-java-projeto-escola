17d802045fa394bfffe7f7b6607924a491c74d13
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-07-12 08:54:59 -0300

    feat: criação do Value Object CPF + teste VO e-mail

--- src/main/java/br/com/alura/Email.java
+++ src/main/java/br/com/alura/Email.java
@@ -14,4 +14,8 @@ public class Email {
         this.endereco = endereco;
     }
 
+    public String getEndereco() {
+        return endereco;
+    }
+
 }
---
+++ src/test/java/br/com/alura/EmailTest.java
@@ -0,0 +1,28 @@
+package br.com.alura;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class EmailTest {
+
+    @Test
+    void naoDeveCriarEmailsComEnderecosInvalidos() {
+        
+        assertThrows(IllegalArgumentException.class, () -> new Email(null));
+
+        assertThrows(IllegalArgumentException.class, () -> new Email(""));
+
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailInvalido"));
+    }
+
+    @Test
+    void deveCriarEmailValido() {
+        
+        Email emailValido = new Email("emailvalido@email.com");
+        assertTrue(emailValido.getEndereco().matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"));
+    }
+}
